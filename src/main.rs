11fed33c166ed3bfb55b032ba3371aad42a0106d
Frederik B <freddy@flashpad>
2017-10-15 15:15:13 +0200

    add options handling

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,68 @@ use std::io;
 use std::io::Write;
 use std::io::Read;
 use std::io::BufRead;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::process;
 use std::thread;
+extern crate getopts;
+use getopts::Options;
 
 extern crate mio;
 use mio::{Events, Ready, Poll, PollOpt, Token};
 use mio::net::TcpStream;
 
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [-46Chv] [destination] [port]", program);
+    print!("{}", opts.usage(&brief));
+}
 fn main() {
     // arg parsing
     let args: Vec<_> = env::args().collect();
-    if args.len() != 3 {
-        println!("Usage: {} <ip> <port>", args[0]);
-        process::exit(1);
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optflag("v", "verbose", "give more verbose output");
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("C", "crlf", "Send CRLF as line ending");
+    opts.optflag("4", "four", "use IPv64 (default)");
+    opts.optflag("6", "six", "use IPv6");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m },
+        Err(e) => { panic!(e.to_string()) }
+    };
+    // parse flags
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
     }
-    let port: u32 = match args[2].parse() {
+    if matches.opt_present("v") {
+        let verbose = true;
+    } else {
+        let verbose = false;
+    }
+    let ending : &str;
+    if matches.opt_present("C") {
+        ending = "\x0D\x0A";
+    } else {
+        ending = "\x0A";
+    }
+    let mut v6 = false;
+    if matches.opt_present("6") {
+        v6 = true;
+    }
+
+    // parse hots/port
+    if !matches.free.len() == 2 {
+        print_usage(&program, opts);
+        return;
+    };
+    let host = matches.free[0].clone();
+    let port: u32 = match matches.free[1].parse() {
         Ok(p) => p,
         Err(_) => { println!("invalid port number"); process::exit(1); }
     };
-    let host = &args[1];
-    let ending = "\x0A";
+
+
+
     ///////////////////////////////////////////////////////////////
 
     // Pick a token that will not be used by any other socket and
